7421cc17b2a61e40029c08135cba71febf2c1e5e
Christopher Webber <cwebber@getchef.com>
2014-06-26 05:12:47 -0700

    Makes version number follow semver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs libboost"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2"
+version          "0.2.0"
 name             "boost"
 provides         "boost"
 recipe           "boost", "Installs libboost-dev"
