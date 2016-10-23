ee6c51bd4b838862465a600d742a1fca2ee1d0ec
Tim Smith <tsmith@chef.io>
2016-10-23 12:10:26 -0700

    Convert to inspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,6 +1,4 @@
-require_relative './spec_helper'
-
-describe 'boost::default recipe' do
+control 'boost::default recipe' do
   describe file('/usr/include/boost') do
     it { should be_directory }
   end
