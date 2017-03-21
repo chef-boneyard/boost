35b1746e83d1ee6a2917f33e5f2e6dd993d97cbd
Tim Smith <tsmith@chef.io>
2017-03-21 15:50:08 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'boost'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs the Boost C++ Libraries'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
